72a5c9bce91d1ce4fcd1e42c5608fae0b8e56961
FelixGauer <felixgauer@gmx.net>
2024-09-17 16:27:13 +0200

    Comments to WaveSpawner.cs

--- WarDogs/Assets/Scripts/Enemy/WaveSystem/WaveSpawner.cs
+++ WarDogs/Assets/Scripts/Enemy/WaveSystem/WaveSpawner.cs
@@ -28,13 +28,15 @@ public class WaveSpawner : NetworkBehaviour
     public int enemiesToSpawn;
     public int wave = 1;
     public List<GameObject> activeSpawnPoints = new List<GameObject>();
-
+    
+    // Delete
     [Header("Boss Wave Settings")]
     public EnemyAIScriptableObject bossEnemy; //Change to array if more bosses are introduced
     public int reduceSpawnByDivision = 2; //divide the spawn by this number to reduce the number of enemies spawned
     
     [Header("EnemySpawning")]
     public int additionalMaxEnemies = 2; //Gamedesigners vars
+    // Spawning should be time based, to be discussed
     public int minEnemies; //Spawn next wave when only this many enemies are left
     public int additionalEnemiesPerWave = 2;
     public int additionalEnemiesPerBreaches = 2;
@@ -78,6 +80,9 @@ public class WaveSpawner : NetworkBehaviour
             hasAudioPlayed = false;
         }
         
+        // Felix: I'm very critical about this line, is going to be changed at some point.
+        // Felix: I would like to be able to trigger the next enemy wave on button press in Gamemode, i.E. after running the game, esc. create lobby. Then press "y" + "k" to trigger the next wave manually.
+        
         if (enemiesAlive <= minEnemies)
         {
             if (!waveIncremented)
@@ -86,13 +91,14 @@ public class WaveSpawner : NetworkBehaviour
                 waveNum.text = "Wave: " + wave.ToString(); //Display number of waves
                 waveIncremented = true;
             }
-
+            // Should be deleted right now
             if (wave % 10 == 0 && activeSpawnPoints.Count >= 12) //if active spawn points are more than 50% boss wave 
             {
                 randomiseSpawn = false;
                 SpawnEnemies(enemiesToSpawn / reduceSpawnByDivision , bossEnemy);
                 return;
             }
+            // this too
             else if (wave % 5 == 0) //Particular Enemy Wave
             {
                 randomiseSpawn = false;
@@ -116,13 +122,22 @@ public class WaveSpawner : NetworkBehaviour
     private void SpawnEnemies(int numberOfEnemies, EnemyAIScriptableObject enemyType)
     {
         // maxEnemies = maxEnemies + additionalMaxEnemies;
+
+        // Felix: We should make sure we can put in facorizable values at some point that are being rounded. e.G. (wave * additionalEnenmiesPerWave 0.2) 
+        // Felix: Another suggestion is to add another var int TestingAdjustment and add it in like this:
+        // enemiesToSpawn = ((minEnemies + (wave * additionalEnemiesPerWave) + (activeSpawnPoints.Count * additionalEnemiesPerBreaches)) + TestingAdjustment - enemiesAlive);
+        // Felix: During testing I can press a button (e.G. "+" and "-") to add +10 or -10 to enemies to spawn, adjust the amount for the next wave INGAME;
+        // Felix: This way we'd have much more freedom in quickly adjusting and I didn't have to build a new game for each round of testing.
+
         enemiesToSpawn = ((minEnemies + (wave * additionalEnemiesPerWave) + (activeSpawnPoints.Count * additionalEnemiesPerBreaches)) - enemiesAlive);
 
         if (enemiesToSpawn >= maxEnemies)
         {
             enemiesToSpawn = maxEnemies;
         }
-
+        // Felix: Does PercentageOfEnemiesToTargetPermPart controll the amount of enemies that targets them?
+        // Felix: Rechecked: All good. Note for later. This only works with values <= 1.
+        // Felix: Stuff like this is probably going to move to the state machine later on.
         int permanentPartTargetCount = Mathf.CeilToInt(numberOfEnemies * percentageOfEnemiesToTargetPermanentPart);
         int playerTargetCount = numberOfEnemies - permanentPartTargetCount;
         
