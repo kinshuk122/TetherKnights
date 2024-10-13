94a0bc8c1a1658817485f04894aa9be8b7cc7aa0
kinshuk <kinshuk364@gmail.com>
2024-10-13 05:08:02 +0200

    Turret added, pick and drop added
    Please kill me

--- WarDogs/Assets/Scripts/Player/PlayerStats.cs
+++ WarDogs/Assets/Scripts/Player/PlayerStats.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using StarterAssets;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerStats : NetworkBehaviour
 {
@@ -19,10 +20,21 @@ public class PlayerStats : NetworkBehaviour
     [Header("Healing")]
     public float secondsAfterHealAllowed;
     private float timeSinceLastDamage;
+
+    [Header("Pickup&Place")] 
+    [SerializeField] private LayerMask pickableLayerMask;
+    [SerializeField] [Min(1)] private float hitRange;
+    [SerializeField] private Transform playerCameraTransform;
+    [SerializeField] private Transform pickUpParent;
+    [SerializeField] private GameObject inHandItem;
+    public bool isHolding;
+    PlayerInput playerInput;
+    private RaycastHit hit;
     
     private void Awake()
     {
         GameManager.instance.currentAlivePlayers++;
+        playerInput = GetComponentInParent<PlayerInput>();
     }
 
     private void Update()
@@ -46,8 +58,6 @@ public class PlayerStats : NetworkBehaviour
                 GameManager.instance.currentAlivePlayers--;
                 isDead.Value = true;
                 allowToHeal.Value = false;
-                // fpsControllerScript.enabled = false;
-                // gunsScript.enabled = false;
                 
                 respawnTimer.Value = respawnTimeIncrease.Value;
             }
@@ -58,8 +68,77 @@ public class PlayerStats : NetworkBehaviour
                 RespawnCountdown(); 
             }
         }
+        
+        // Pickup and place logic
+        if (hit.collider != null)
+        {
+            hit.collider.GetComponentInParent<Highlight>()?.ToggleHighlight(false);
+        }
+        
+        Debug.DrawRay(playerCameraTransform.position, playerCameraTransform.forward * hitRange, Color.green);
+        if(Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out hit, hitRange, pickableLayerMask))
+        {
+            hit.collider.GetComponentInParent<Highlight>()?.ToggleHighlight(true);
+            if (playerInput.actions["Pickup"].WasPressedThisFrame() && !isHolding)
+            {
+                Pickup();
+            }
+        }
+        
+        if (playerInput.actions["Pickup"].WasReleasedThisFrame())
+        {
+            if (isHolding)
+            {
+                Drop();
+            }
+        }
     }
 
+    private void Pickup()
+    {
+        if(hit.collider != null)
+        {
+            if (((1 << hit.collider.gameObject.layer) & pickableLayerMask) != 0)
+            {
+                Rigidbody rb = hit.collider.GetComponentInParent<Rigidbody>();
+                Debug.Log(hit.collider.name);
+                inHandItem = hit.collider.transform.parent.gameObject;
+                inHandItem.transform.SetParent(pickUpParent.transform,true);
+                inHandItem.transform.localPosition = Vector3.zero;
+                inHandItem.transform.localRotation = Quaternion.identity;
+                isHolding = true;
+                if(rb != null)
+                {
+                    rb.isKinematic = true;
+                    Debug.Log("Picked up " + inHandItem.name);
+                }
+                return;
+            }
+        }
+    }
+
+    private void Drop()
+    {
+        Vector3 dropPosition = playerCameraTransform.position + playerCameraTransform.forward * 2.0f;
+        RaycastHit floorHit;
+        if (Physics.Raycast(dropPosition, Vector3.down, out floorHit, Mathf.Infinity))
+        {
+            dropPosition.y = floorHit.point.y;
+        }
+
+        Rigidbody rb = inHandItem.gameObject.GetComponentInParent<Rigidbody>();
+        
+        if(rb != null)
+        {
+            rb.isKinematic = false;
+        }
+        
+        inHandItem.transform.position = dropPosition;
+        inHandItem.transform.SetParent(null);
+        inHandItem = null;
+        isHolding = false;
+    }
+    
     [ServerRpc(RequireOwnership = false)]
     public void TakeDamageServerRpc(float damage)
     {
@@ -106,16 +185,5 @@ public class PlayerStats : NetworkBehaviour
         health.Value = maxHealth;
         allowToHeal.Value = true;
     }
-    
-    // [ClientRpc]
-    // private void RespawnClientRpc()
-    // {
-    //     if (!IsLocalPlayer) return;
-    //
-    //     fpsControllerScript.enabled = true;
-    //     gunsScript.enabled = true;
-    //
-    //     RespawnServerRpc();
-    // }
     
 }
